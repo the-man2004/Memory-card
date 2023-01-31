6fe759028fc1935aee6a4dd985f68744fae120a5
the-man2004 <heinrichhb111@gmail.com>
2023-01-31 13:25:25 -0800

    created boilerplate code and imported Items.js

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Items from "./Items.js";
+
+function Main(props) {
+
+    return (
+        <div className="container">
+            <Items />
+        </div>
+    )
+}
+
+export default Main;
