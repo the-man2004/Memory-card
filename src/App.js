0e11a26e193551924d2499c473d4ea4376fc4828
the-man2004 <heinrichhb111@gmail.com>
2023-01-31 13:23:21 -0800

    imported and displayed Header and Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
+import "./styles/app.css"
 
 function App() {
+  //const [score, setScore] = useState(0);
+  //const [highScore, setHighScore] = useState(0);
+
   return (
     <div className="App">
-      Let's gooooo!!!
+      <Header />
+      <Main />
     </div>
   );
 }
