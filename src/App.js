993ad8cc8f2c9c75346aac1fa507b32e41bd2fe4
the-man2004 <heinrichhb111@gmail.com>
2023-02-01 22:06:51 -0800

    cleaned up unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import "./styles/app.css"
 
 function App() {
-  //const [score, setScore] = useState(0);
-  //const [highScore, setHighScore] = useState(0);
 
   return (
     <div className="App">
