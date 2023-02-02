6333d604e54ecf5e9c71be49c4cc1c3fc66838fb
the-man2004 <heinrichhb111@gmail.com>
2023-02-01 22:11:38 -0800

    made different combos(not sure if necessary)

---
+++ src/components/randomCombination.js
@@ -0,0 +1,76 @@
+
+export const cardOne = [
+    {
+        title: "num1",
+        url: "undefined",
+    },
+    {
+        title: "num2",
+        url: "undefined",
+    },
+    {
+        title: "num3",
+        url: "undefined",
+    },
+    {
+        title: "num4",
+        url: "undefined",
+    }
+];
+
+export const cardTwo = [
+    {
+        title: "num4",
+        url: "undefined",
+    },
+    {
+        title: "num3",
+        url: "undefined",
+    },
+    {
+        title: "num2",
+        url: "undefined",
+    },
+    {
+        title: "num1",
+        url: "undefined",
+    }
+];
+
+export const cardThree = [
+    {
+        title: "num3",
+        url: "undefined",
+    },
+    {
+        title: "num4",
+        url: "undefined",
+    },
+    {
+        title: "num1",
+        url: "undefined",
+    },
+    {
+        title: "num2",
+        url: "undefined",
+    }
+];
+
+export const cardFour = [
+    {
+        title: "num1",
+        url: "undefined",
+    },
+    {
+        title: "num4",
+        url: "undefined",
+    },
+    {
+        title: "num2",
+        url: "undefined",
+    },
+    {
+        title: "num3",
+        url: "undefined",
+    }
+];
